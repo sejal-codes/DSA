31d5c2169fae92a0e8245ea20d131dff7adbbcf7
sejal-codes <ersejal.solanki@gmail.com>
2026-04-20 23:24:50 +0530

    Create recrusive_sumeven.c

---
+++ recrusive_sumeven.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int findsum(int n){
+    if(n==1){
+        return 2;
+    }
+    else
+    return (2*n)+findsum(n-1);
+}
+int main() {
+   int n;
+   printf("enter the value of N:");
+   scanf("%d",&n);
+   printf("the sum is:%d",findsum(n));
+ return 0;
+}
